2b9764abd40e54b84b3366e03b23c2a6f525d1c2
reach.pheun <reachpheun@vital.com.kh>
2022-11-02 11:22:44 +0700

    edit order product

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,201 +1,321 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Larave Generate Invoice PDF - Nicesnippest.com</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset("css/order.css") }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
-<style type="text/css">
-    body{
-        font-family: 'Roboto Condensed', sans-serif;
-    }
-    .m-0{
-        margin: 0px;
-    }
-    .p-0{
-        padding: 0px;
-    }
-    .pt-5{
-        padding-top:5px;
-    }
-    .mt-10{
-        margin-top:10px;
-    }
-    .text-center{
-        text-align:center !important;
-    }
-    .w-100{
-        width: 100%;
-    }
-    .w-50{
-        width:50%;   
-    }
-    .w-85{
-        width:85%;   
-    }
-    .w-15{
-        width:15%;   
-    }
-    .logo img{
-        width:45px;
-        height:45px;
-        padding-top:30px;
-    }
-    .logo span{
-        margin-left:8px;
-        top:19px;
-        position: absolute;
-        font-weight: bold;
-        font-size:25px;
-    }
-    .gray-color{
-        color:#5D5D5D;
-    }
-    .text-bold{
-        font-weight: bold;
-    }
-    .border{
-        border:1px solid black;
-    }
-    table tr,th,td{
-        border: 1px solid #d2d2d2;
-        border-collapse:collapse;
-        padding:7px 8px;
-    }
-    table tr th{
-        background: #F4F4F4;
-        font-size:15px;
-    }
-    table tr td{
-        font-size:13px;
-    }
-    table{
-        border-collapse:collapse;
-    }
-    .box-text p{
-        line-height:10px;
-    }
-    .float-left{
-        float:left;
-    }
-    .total-part{
-        font-size:16px;
-        line-height:12px;
-    }
-    .total-right p{
-        padding-right:20px;
-    }
-</style>
 <body>
-<div class="head-title">
-    <h1 class="text-center m-0 p-0">Invoice</h1>
-</div>
-<div class="add-detail mt-10">
-    <div class="w-50 float-left mt-10">
-        <p class="m-0 pt-5 text-bold w-100">Invoice Id - <span class="gray-color">#6</span></p>
-        <p class="m-0 pt-5 text-bold w-100">Order Id - <span class="gray-color">162695CDFS</span></p>
-        <p class="m-0 pt-5 text-bold w-100">Order Date - <span class="gray-color">03-06-2022</span></p>
+  <nav class="navbar mb-2">
+    <a class="navbar-brand" href="#">
+      <img src="{{asset("static_images/logo.png")}}"  class="logo-brand">
+      Sokha Store
+    </a>
+
+  </nav>
+  <div class="container-fluid " id="pro-order">
+    <ul class="scrol-nav col-12">
+        <li>
+            <a class="nav-item nav-link cat-item cat-active category-filter" href="#" data-id="0">ទាំងអស់</a>
+        </li>
+        @foreach ($category as $item)
+        <li>
+            <a class="nav-item nav-link cat-item category-filter " href="#" data-id="{{ $item->id }}">{{ $item->title }}</a>
+        </li>
+        @endforeach
+
+
+    </ul>
+    <div class="col-12 product-list ">
+      <div class="row product-wrap">
+        @foreach ($products as $item )
+        <div class="grid-product">
+          <img src="{{ $item->image ? asset("uploads/product/".$item->image ) : asset("image/no_pro.png")}}" >
+
+          <div style="height: 50px; text-overflow: ellipsis;" >{{ $item->name }}</div>
+          <div class="btn-order">
+            <span class="value-order" style="top:-5px;right:-5px" data-id="{{ $item->id }}" data-toggle="{{ $item->name }}" data-show="{{ $item->unit_price }}">0</span>
+            <button class="btn-descrease bg-warning" style="outline: none; color:azure"><i class="fa fa-minus"></i></button>
+            <button class="btn-increase bg-info" style="outline: none;  color:azure" ><i class="fa fa-plus"></i></button>
+          </div>
+        </div>
+        @endforeach
+
+      </div>
     </div>
-    <div class="w-50 float-left logo mt-10">
-        <img src="https://www.nicesnippets.com/image/imgpsh_fullsize.png"> <span>Nicesnippets.com</span>     
+    <div class="footer-submit">
+      មើលការកុម៉ង់
     </div>
-    <div style="clear: both;"></div>
-</div>
-<div class="table-section bill-tbl w-100 mt-10">
-    <table class="table w-100 mt-10">
-        <tr>
-            <th class="w-50">From</th>
-            <th class="w-50">To</th>
-        </tr>
-        <tr>
-            <td>
-                <div class="box-text">
-                    <p>Gujarat</p>
-                    <p>360004</p>
-                    <p>Near Haveli Road,</p>
-                    <p>Lal Darvaja,</p>
-                    <p>India</p>
-                    <p>Contact : 1234567890</p>
-                </div>
-            </td>
-            <td>
-                <div class="box-text">
-                    <p>Rajkot</p>
-                    <p>360012</p>
-                    <p>Hanumanji Temple,</p>
-                    <p>Lati Ploat</p>
-                    <p>Gujarat</p>
-                    <p>Contact : 1234567890</p>
-                </div>
-            </td>
-        </tr>
-    </table>
-</div>
-<div class="table-section bill-tbl w-100 mt-10">
-    <table class="table w-100 mt-10">
-        <tr>
-            <th class="w-50">Payment Method</th>
-            <th class="w-50">Shipping Method</th>
-        </tr>
-        <tr>
-            <td>Cash On Delivery</td>
-            <td>Free Shipping - Free Shipping</td>
-        </tr>
-    </table>
-</div>
-<div class="table-section bill-tbl w-100 mt-10">
-    <table class="table w-100 mt-10">
-        <tr>
-            <th class="w-50">SKU</th>
-            <th class="w-50">Product Name</th>
-            <th class="w-50">Price</th>
-            <th class="w-50">Qty</th>
-            <th class="w-50">Subtotal</th>
-            <th class="w-50">Tax Amount</th>
-            <th class="w-50">Grand Total</th>
-        </tr>
-        <tr align="center">
-            <td>$656</td>
-            <td>Mobile</td>
-            <td>$204.2</td>
-            <td>3</td>
-            <td>$500</td>
-            <td>$50</td>
-            <td>$100.60</td>
-        </tr>
-        <tr align="center">
-            <td>$656</td>
-            <td>Mobile</td>
-            <td>$254.2</td>
-            <td>2</td>
-            <td>$500</td>
-            <td>$50</td>
-            <td>$120.00</td>
-        </tr>
-        <tr align="center">
-            <td>$656</td>
-            <td>Mobile</td>
-            <td>$554.2</td>
-            <td>5</td>
-            <td>$500</td>
-            <td>$50</td>
-            <td>$100.00</td>
-        </tr>
-        <tr>
-            <td colspan="7">
-                <div class="total-part">
-                    <div class="total-left w-85 float-left" align="right">
-                        <p>Sub Total</p>
-                        <p>Tax (18%)</p>
-                        <p>Total Payable</p>
-                    </div>
-                    <div class="total-right w-15 float-left text-bold" align="right">
-                        <p>$20</p>
-                        <p>$20</p>
-                        <p>$330.00</p>
-                    </div>
-                    <div style="clear: both;"></div>
-                </div> 
-            </td>
-        </tr>
-    </table>
-</div>
+
+  </div>
+
+  <div class="invoice-page">
+    <div class="invoice-header">
+        <div class="btn-back"><i class="fa fa-angle-left fa-lg"></i></div>
+        <h2>វិកយប័ត្រ</h2>
+
+    </div>
+    <div class="cus-profile">
+      <label>បញ្ចូលលេខទូរស័ព្ទ</label>
+        <div class="pro-more" style="display: grid">
+          
+          <input type="number" name="phone" id="phone" style="  
+            border: 1px solid green;
+            
+            "  
+          >
+          <span style="font-size: 14px; color:red; display:none" id="warning-phone">សូមបំពេញលេខទូរស័ព្ទដែលត្រឹមត្រូវ</span>
+        </div>
+        
+    </div>
+   
+    <div class="invoice-body">
+        <div class="list-order">
+            <table class="table table-borderless">
+                <tbody id="list-data">
+
+                </tbody>
+              </table>
+              <div style="float: right;margin-right:20px; border-top: 1px solid;padding-top: 10px;" id="total">
+
+              </div>
+        </div>
+    </div>
+    <div class="btn-action">
+        <button  type="button" onclick="{window.location.reload()}">បោះបង់</button>
+        <button type="button" class="" id="pro-ordering">ទិញ</button>
+    </div>
+  </div>
+
+</body>
+
+<script src="{{ asset('js/bot.js') }}"></script>
+<script >
+    data_store_array ={};
+    final_array =[]
+    final_data =[];
+    response_message =[];
+  $(document).ready(function(){
+
+
+    $('body').on('click', '.btn-descrease', function(){
+        var pro_id = $(this).siblings(".value-order").attr("data-id")
+      var value = $(this).siblings(".value-order").text()
+      if (value >0){
+        value --;
+
+      }
+      $(this).siblings(".value-order").html(value)
+      if (value== 0){
+        delete data_store_array[pro_id]
+
+      }
+
+      if (Object.keys(data_store_array).length <= 0){
+        $(".footer-submit").css({display:"none"})
+      }
+     
+    })
+
+    $('body').on('click', '.btn-increase', function(){
+        var pro_id = $(this).siblings(".value-order").attr("data-id")
+      var value = $(this).siblings(".value-order").text()
+      if (value <100){
+        value ++;
+
+      }
+      if (value > 0){
+        $(".footer-submit").css({display:"block"})
+      }
+
+      $(this).siblings(".value-order").html(value)
+
+      data_store_array[pro_id] = value
+    });
+
+
+    // invoice page
+
+    $(".footer-submit").on('click', function(){
+        $("#pro-order").css({"display":"none", "transition":'1s'})
+        $(".invoice-page").css({"display":"block" , "transition":'1s'});
+        sub_key = Object.keys(data_store_array)
+        sub_val = Object.values(data_store_array)
+        final_array =[]
+        data_id = [];
+        for(var i=0; i<sub_key.length; i++){
+            data_id.push(parseInt(sub_key[i]))
+          final_array.push(
+            {
+              "pro_id" :sub_key[i],
+              "value" : data_store_array[sub_key[i]]
+            }
+          )
+        }
+
+        pro_name =[];
+        pro_price=[];
+
+        $.ajax({
+        url:"{{ route('order.price_byid') }}",
+        data:{
+            id:data_id
+        },
+        success:function(res){
+            res.data.map((obj)=>{
+                pro_name.push(obj.name);
+                pro_price.push(obj.unit_price);
+            })
+            showDataList(final_array, pro_name, pro_price)
+
+            }
+        })
+        // getPriceDataById(data_id)
+    })
+    $(".btn-back").on('click', function(){
+        $("#pro-order").css({"display":"block", "transition":'1s'})
+        $(".invoice-page").css({"display":"none"})
+    });
+
+    // show product by category ==================================================
+
+    $(".category-filter").on('click', function(){
+        $(".category-filter").css({"backgroundColor":"bisque"})
+        $(this).css({"backgroundColor":"cyan"})
+        var cat_id = $(this).attr("data-id")
+        $.ajax({
+          url: "{{ route('order.filter_cat') }}",
+          data:{
+            cate:cat_id
+          },
+
+          success:function(res){
+            var list_html=""
+            res.map((obj)=>{
+                if (obj.image['img_name'] == "")
+                {
+
+                    console.log("No Product")
+                    url = "{{ asset('image/no_pro.png') }}";
+                }
+                else {
+                    url = "{{ asset('uploads/product') }}"+"/"+obj.image;
+                }
+                if (obj.id == data_store_array[obj.id]){
+                }
+                sub_data = Object.keys(data_store_array)
+                pre_value =0
+                for (var i=0; i<sub_data.length; i++){
+                    if (sub_data[i] == obj.id){
+                        pre_value=data_store_array[obj.id]
+                    }
+                }
+
+
+                list_html +='<div class="grid-product">'+
+                            `<img src="${url}" >`+
+                            `<marquee scrollamount="1">${obj.name}</marquee>`+
+                            '<div class="btn-order">'+
+                                '<span class="value-order" data-id="'+obj.id+'">'+pre_value+'</span>'+
+                                '<button class="btn-descrease bg-warning" style="outline: none; color:azure"><i class="fa fa-minus"></i></button>'+
+                                '<button class="btn-increase bg-info" style="outline: none;  color:azure" ><i class="fa fa-plus"></i></button>'+
+                            '</div>'+
+                            '</div>'
+            })
+            $(".product-wrap").html(list_html)
+          }
+        });
+    })
+  });
+
+  function showDataList(list, list1,list2){
+    list_data="";
+    total =0;
+    list.forEach((element, index )=> {
+
+        final_data.push({
+            "pro_id":element.pro_id,
+            "qty":element.value,
+            "sub_total":(list2[index]*element.value).toFixed(2)
+
+        });
+      total +=list2[index]*element.value;
+
+      list_data +=
+        ' <tr >'+
+            '<th scope="row">'+(index+1)+'</th>'+
+            '<td class="col-6">'+list1[index]+'</td>'+
+            '<td class="col-2">'+element.value+'</td>'+
+            '<td class="col-2">'+list2[index]+'$</td>'+
+            '<td class="col-2">'+(list2[index]*element.value).toFixed(2)+'$</td>'+
+          '</tr>'
+
+      response_message.push({
+        name : list1[index],
+        qty : element.value,
+        price : list2[index],
+        sub_total :(list2[index]*element.value).toFixed(2)
+      });
+
+    });
+    $("#list-data").html(list_data)
+    $("#total").html(
+        '<h5>'+total.toFixed(2)+'$</h5>'
+    )
+  }
+
+
+
+$("#pro-ordering").on('click', function(){
+
+    numberphone = $("#phone").val();
+    var chat_id =0;
+    $.ajax({
+        url:"{{ route('order.customer') }}",
+        type:"get",
+        data:{
+            phone :numberphone
+        },
+        success:function(res){
+            chat_id = res.success.chatid
+            if (res.success === false){
+                $("#warning-phone").css({display:"block"})
+                // if(confirm("លេខរបស់លោកអ្នកមិនត្រឹមត្រូវទេ សូមធ្វើការចុះឈ្មោះងាយៗ តាមលេតេក្រាម ដោយចុចពាក្យថាខាងក្រោម")){
+                //     window.open("https://t.me/sokha_stores_bot/bot?start=sokhastock");
+                // }
+            }else{
+                    $.ajax({
+                    url:"{{ route('order.product') }}",
+                    type:"POST",
+                    data:{
+                        data:final_data,
+                        custo:res.success.id,
+                        _token: '{{csrf_token()}}'
+                    },
+                    success:function(data){
+                        status_  = senMessageBot(chat_id, response_message);
+                        final_array=[];
+                        final_data =[];
+                        setTimeout(() => {
+                              
+                            }, 10000);
+                      
+                    }
+                })
+            }
+        }
+    })
+
+})
+
+
+</script>
+
 </html>
